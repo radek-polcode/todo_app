202809adbf51dca12a8658ce86855f4c20c972c3
Radoslaw Stolarski <stolarz92@gmail.com>
2017-03-18 13:30:33 +0100

    Authentication

--- angular-token-auth/src/app/app.module.ts
+++ angular-token-auth/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { AuthDialogComponent } from './auth-dialog/auth-dialog.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
 import {AuthService} from "./services/auth.service";
+import { ProfileComponent } from './profile/profile.component';
+import {AuthGuard} from "./guards/auth.guard";
 
 @NgModule({
   declarations: [
@@ -21,7 +23,8 @@ import {AuthService} from "./services/auth.service";
     ToolbarComponent,
     AuthDialogComponent,
     LoginFormComponent,
-    RegisterFormComponent
+    RegisterFormComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +33,7 @@ import {AuthService} from "./services/auth.service";
     AppRoutingModule,
     MaterializeModule
   ],
-  providers: [ Angular2TokenService, AuthService ],
+  providers: [ Angular2TokenService, AuthService, AuthGuard ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
