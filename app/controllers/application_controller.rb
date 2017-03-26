c881faecb22ef4aabf52875f8e4e1e31bb016854
Radoslaw Stolarski <stolarz92@gmail.com>
2017-03-26 17:50:33 +0200

    Add methods to concerns + add concerns to app Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::Serialization
+  include Response
+  include ExceptionHandler
 end
