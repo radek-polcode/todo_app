f106dd5b40635e2e38f2e7bb9517b87cf0566b3e
Radoslaw Stolarski <stolarz92@gmail.com>
2017-03-26 17:49:32 +0200

    Config changes

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module TodoApp
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options]
       end
     end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_dispatch.tld_length = 0
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
