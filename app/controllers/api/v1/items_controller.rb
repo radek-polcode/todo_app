8446eca3721dd49b0ee93cddac028cf455c7b163
Radoslaw Stolarski <stolarz92@gmail.com>
2017-03-28 23:18:08 +0200

    Fix controllers namespaces

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,6 @@
-module V1
+module Api::V1
   class ItemsController < ApiController
-    before_action :authenticate_v1_user!
+    # before_action :authenticate_v1_user!
     before_action :set_item, only: [:show, :update, :destroy]
 
     # GET /items
